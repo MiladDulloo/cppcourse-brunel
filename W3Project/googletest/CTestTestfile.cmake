e58a371f3b13ed532334fbd5bbabe0921a198c79
MiladDulloo <32447544+MiladDulloo@users.noreply.github.com>
2017-11-05 19:56:20 +0100

    Add files via upload

---
+++ W3Project/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/INTRANET/dulloo/myfiles/CS116-2017-milad-dulloo-/W3Project/googletest
+# Build directory: /home/INTRANET/dulloo/myfiles/CS116-2017-milad-dulloo-/W3Project/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
